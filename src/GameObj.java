01f4474f9bf6f3ecafde9d59b73e9cf5c8e974bf
ChickenGoddess <benevolentdarkness@gmail.com>
2016-10-21 01:02:07 -0400

    Add stuffs

--- src/GameObj.java
+++ src/GameObj.java
@@ -1,3 +1,6 @@
+
+import java.awt.Graphics;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,51 @@
  *
  * @author Chicken
  */
-public interface GameObj {
+public abstract class GameObj {
     
+    protected int x;
+    protected int y;
+    protected ID id;
+    protected int velX;
+    protected int velY;
+    
+    public GameObj(int x, int y, ID id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+    
+    public abstract void tick();
+    public abstract void render(Graphics g);
+    
+    public int getPosX(){
+        return x;
+    }
+    public int getPosY(){
+        return y;
+    }
+    public ID getID(){
+        return id;
+    }
+    public int getVelX(){
+        return velX;
+    }
+    public int getVelY(){
+        return velY;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public void setVelX(int velX){
+        this.velX = velX;
+    }
+    public void setVelY(int velY){
+        this.velY = velY;
+    }
+    public void setID(ID id){
+        this.id = id;
+    }
 }
